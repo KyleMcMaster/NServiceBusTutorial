857428fce4ae0e1c010a0cf6288614bbf6a91d89
KyleMcMaster <11415127+KyleMcMaster@users.noreply.github.com>
2024-05-14 21:15:16 -0400

    add saga host startup configuration

--- src/NServiceBusTutorial.Saga/Program.cs
+++ src/NServiceBusTutorial.Saga/Program.cs
@@ -1,4 +1,14 @@
-﻿var builder = Host.CreateDefaultBuilder(args);
+﻿using NServiceBus;
+
+var builder = Host.CreateDefaultBuilder(args);
+
+builder.UseNServiceBus(context => 
+{
+  var endpointConfiguration = new EndpointConfiguration("contributors-saga");
+  endpointConfiguration.UseTransport<LearningTransport>();
+
+  return endpointConfiguration;
+});
 
 var host = builder.Build();
 host.Run();
