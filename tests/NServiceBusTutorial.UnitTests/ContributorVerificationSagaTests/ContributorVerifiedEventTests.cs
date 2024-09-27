5e5de548e4e2d97d4464cce04de99e3ac9bca58d
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2024-09-26 21:57:21 -0400

    Update ContributorVerifiedEventTests for upcoming Scenario tests, Add StartContributorVerificationCommand tests for saga

--- tests/NServiceBusTutorial.UnitTests/ContributorVerificationSagaTests/ContributorVerifiedEventTests.cs
+++ tests/NServiceBusTutorial.UnitTests/ContributorVerificationSagaTests/ContributorVerifiedEventTests.cs
@@ -1,8 +1,5 @@
 ﻿using FluentAssertions;
-using FluentAssertions.Execution;
 using NServiceBus.Testing;
-using NServiceBusTutorial.Core.ContributorAggregate;
-using NServiceBusTutorial.Core.ContributorAggregate.Commands;
 using NServiceBusTutorial.Core.ContributorAggregate.Events;
 using NServiceBusTutorial.Saga;
 using Xunit;
@@ -20,30 +17,5 @@ public class ContributorVerifiedEventTests
     var result = await saga.Handle(message, context);
 
     result.Completed.Should().BeTrue();
-  }
-
-  [Fact]
-  public async Task ShouldInitializeSagaAndMarkAsCompleted()
-  {
-    int expectedContributorId = 1;
-    var startMessage = new StartContributorVerificationCommand()
-    {
-      ContributorId = expectedContributorId
-    };
-    var message = new ContributorVerifiedEvent()
-    {
-      ContributorId = expectedContributorId
-    };
-    var saga = new TestableSaga<ContributorVerificationSaga, ContributorVerificationSagaData>();
-    var context = new TestableMessageHandlerContext();
-
-    var result = await saga.Handle(startMessage, context);
-    using var assertionScope = new AssertionScope();
-    result.SagaDataSnapshot.ContributorId.Should().Be(expectedContributorId);
-
-    result = await saga.Handle(message, context);
-    result.Completed.Should().BeTrue();
-    var timeoutMessage = result.FindTimeoutMessage<ContributorVerificationSagaTimeout>();
-    timeoutMessage.Should().NotBeNull();
   }
 }
---
+++ tests/NServiceBusTutorial.UnitTests/ContributorVerificationSagaTests/StartContributorVerificationCommandTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using NServiceBus.Testing;
+using NServiceBusTutorial.Core.ContributorAggregate.Commands;
+using NServiceBusTutorial.Saga;
+using Xunit;
+
+namespace NServiceBusTutorial.UnitTests.ContributorVerificationSagaTests;
+
+public class StartContributorVerificationCommandTests
+{
+  [Fact]
+  public async Task ShouldSendVerifyContributorCommand()
+  {
+    var message = new StartContributorVerificationCommand();
+    var saga = new ContributorVerificationSaga
+    {
+      Data = new()
+    };
+    var context = new TestableMessageHandlerContext();
+
+    await saga.Handle(message, context);
+
+    var sentMessage = context.FindSentMessage<VerifyContributorCommand>();
+    sentMessage.Should().NotBeNull();
+  }
+
+  [Fact]
+  public async Task ShouldSetContributorIdOnSagaState()
+  {
+    var message = new StartContributorVerificationCommand()
+    {
+      ContributorId = 4680
+    };
+    var saga = new ContributorVerificationSaga
+    {
+      Data = new()
+    };
+    var context = new TestableMessageHandlerContext();
+
+    await saga.Handle(message, context);
+
+    saga.Data.ContributorId.Should().Be(message.ContributorId);
+  }
+}
